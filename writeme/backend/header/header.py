057e2c230c6c657d75b94c25adb6cfa0323a5f4e
Wendy Leung <wendy.leung@holbertonschool.com>
2019-05-10 18:54:28 +0000

    add headers replace method to headers class

--- writeme/backend/header/header.py
+++ writeme/backend/header/header.py
@@ -11,4 +11,22 @@ class Header:
 
     @staticmethod
     def header(prefix, content):
-        return ''
+        """
+        replace the headers
+
+        Args:
+            prefix - header tag
+            content - the text to markdown
+        Returns:
+            the text with headers replaced
+        """
+        header_list = [{'biggest': '#'}, {'bigger': '##'}, {'big': '###'},
+                       {'small': '####'}, {'smaller', '#####'},
+                       {'smallest': '######'}]
+
+        # check which one in header_list matches the text and replace it with #'s
+        for header_dict in header_list:
+            for key, value in header_dict.items():
+                if key == prefix:
+                    mine = value + content
+                    return mine
