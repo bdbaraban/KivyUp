ba86e9be913bac0600148a2d6ea9fef73b872c63
Samie Azad <samieazad@Samies-MacBook-Pro.local>
2019-05-10 13:57:08 -0700

    Pushing changes for review by Brennan

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """Entry point of app."""
 from kivy.app import App
-from screen_manager.manager import presentation
+from kivy.uix.screenmanager import ScreenManager
+from screen_manager.manager import presentation, WriteMeScreen, ReadMeScreen
 
+
+#presentation
 
 class WRITEMEmd(App):
     def build(self):
-        return presentation
+        sm = ScreenManager()
+        sm.add_widget(WriteMeScreen())
+        sm.add_widget(ReadMeScreen())
+        return sm
 
 
 WRITEMEmd().run()
--- screen_manager/manager.py
+++ screen_manager/manager.py
@@ -2,19 +2,27 @@
 """Defines the screen manager classes."""
 import os.path
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.screenmanager import FadeTransition, ScreenManager, Screen
 
 
-class WriteMeScreen(Screen):
-    pass
-
-
 class ReadMeScreen(Screen):
-    pass
+    writeText = ObjectProperty(None)
+    def textPrint(self):
+        print(self.writeText.text)
+
+
+class WriteMeScreen(Screen):
+    textBlock = ObjectProperty(None)
+    def textHarvest(self):
+        print(self.textBlock.text)
 
 
 class ScreenManagement(ScreenManager):
-    pass
+    shared_text = StringProperty("")
 
 
-presentation = Builder.load_file(os.path.join(os.path.dirname(__file__), "screens.kv"))
+presentation = Builder.load_file(os.path.join(os.path.dirname(__file__),
+                                              "screens.kv"))
+rms = ReadMeScreen()
+rms.textPrint()
