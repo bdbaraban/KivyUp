ac57bb3d42d372357012b742ea35b3134dac7900
Athena Deng <539@holbertonschool.com>
2019-05-10 15:28:39 -0700

    first commit

---
+++ writeme/backend/inline/inline_stuff.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+for in-line markdown stuff
+"""
+
+
+def bold(bold_line):
+    """
+    makes a line bold with markdown
+    """
+    return "**" + bold_line + "**"
+
+
+def italics(italics_line):
+    """
+    makes a line italics with markdown
+    """
+    return "*" + italics_line + "*"
+
+def in_line_code(code):
+    """
+    makes an unformatted line of code
+    """
+    return "`" + code + "`"
+
+def line_break(dankdown_syntax):
+    """
+    inserts a line break
+    """
+    return dankdown_syntax.replace(dankdown_syntax, "\n")
+
+def emoji(emoji_name):
+    """
+    takes the name of an emoji and turns it into markdown syntax emoji
+    """
+    return ":" + emoji_name + ":"
