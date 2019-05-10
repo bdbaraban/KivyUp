006b01aa190cbf6262acc82419066906b4d5e91c
Brennan Baraban <bdbaraban@ucdavis.edu>
2019-05-10 13:35:00 -0700

    Add new line at end of file

--- screen_manager/manager.py
+++ screen_manager/manager.py
@@ -2,19 +2,23 @@
 """Defines the screen manager classes."""
 import os.path
 from kivy.lang import Builder
-from kivy.uix.screenmanager import FadeTransition, ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 class WriteMeScreen(Screen):
+    """Screen for writing WRITME.md syntax."""
     pass
 
 
 class ReadMeScreen(Screen):
+    """Screen for converted markdown."""
     pass
 
 
 class ScreenManagement(ScreenManager):
+    """Main screen manager."""
     pass
 
 
-presentation = Builder.load_file(os.path.join(os.path.dirname(__file__), "screens.kv"))
+presentation = Builder.load_file(os.path.join(os.path.dirname(__file__),
+                                 "screens.kv"))
